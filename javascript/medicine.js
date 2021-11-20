0f9cf5b88920ecd4630ed4730f4895b1740ebe45
tejas <dobariyatejas15@gmail.com>
2021-11-20 10:52:32 +0530

    first commit

---
+++ javascript/medicine.js
@@ -0,0 +1,131 @@
+function myFunction() {
+    document.getElementById("AntiDiabetic1").style.display = "inline";
+    document.getElementById("AntiMalarials1").style.display = "none";
+    document.getElementById("Hormones1").style.display = "none";
+    document.getElementById("Vitamins1").style.display = "none";
+    document.getElementById("BloodRelated1").style.display = "none";
+    document.getElementById("OtherAntiMalarials1").style.display = "none";
+    document.getElementById("AntiDiabetic2").style.display = "block";
+    document.getElementById("Hormones2").style.display = "none";
+    document.getElementById("Vitamins2").style.display = "none";
+    document.getElementById("BloodRelated2").style.display = "none";
+}
+function myFunction1() {
+    document.getElementById("AntiDiabetic1").style.display = "none";
+    document.getElementById("AntiMalarials1").style.display = "inline";
+    document.getElementById("Hormones1").style.display = "none";
+    document.getElementById("Vitamins1").style.display = "none";
+    document.getElementById("BloodRelated1").style.display = "none";
+    document.getElementById("AntiDiabetic2").style.display = "none";
+    document.getElementById("Hormones2").style.display = "none";
+    document.getElementById("Vitamins2").style.display = "none";
+    document.getElementById("BloodRelated2").style.display = "none";
+}
+function myFunction2() {
+    document.getElementById("AntiDiabetic1").style.display = "none";
+    document.getElementById("AntiMalarials1").style.display = "none";
+    document.getElementById("Hormones1").style.display = "inline";
+    document.getElementById("Vitamins1").style.display = "none";
+    document.getElementById("BloodRelated1").style.display = "none";
+    document.getElementById("OtherAntiMalarials1").style.display = "none";
+    document.getElementById("AntiDiabetic2").style.display = "none";
+    document.getElementById("Hormones2").style.display = "block";
+    document.getElementById("Vitamins2").style.display = "none";
+    document.getElementById("BloodRelated2").style.display = "none";
+}
+function myFunction4() {
+    document.getElementById("AntiDiabetic1").style.display = "none";
+    document.getElementById("AntiMalarials1").style.display = "none";
+    document.getElementById("Hormones1").style.display = "none";
+    document.getElementById("Vitamins1").style.display = "inline";
+    document.getElementById("BloodRelated1").style.display = "none";
+    document.getElementById("OtherAntiMalarials1").style.display = "none";
+    document.getElementById("AntiDiabetic2").style.display = "none";
+    document.getElementById("Hormones2").style.display = "none";
+    document.getElementById("BloodRelated2").style.display = "none";
+}
+function myFunction5() {
+    document.getElementById("AntiDiabetic1").style.display = "none";
+    document.getElementById("AntiMalarials1").style.display = "none";
+    document.getElementById("Hormones1").style.display = "none";
+    document.getElementById("Vitamins1").style.display = "none";
+    document.getElementById("BloodRelated1").style.display = "inline";
+    document.getElementById("OtherAntiMalarials1").style.display = "none";
+    document.getElementById("AntiDiabetic2").style.display = "none";
+    document.getElementById("Hormones2").style.display = "none";
+    document.getElementById("Vitamins2").style.display = "none";
+}
+
+function function6() {
+    document.getElementById("HumanInsulinBasal1").style.display = "block";
+    document.getElementById("InsulinAnaloguesBasal1").style.display = "none";
+}
+function function8() {
+    document.getElementById("HumanInsulinBasal1").style.display = "none";
+    document.getElementById("InsulinAnaloguesBasal1").style.display = "block";
+}
+function function9() {
+    document.getElementById("OtherAntiMalarials1").style.display = "block";
+}
+function function10() {
+    document.getElementById("Acth1").style.display = "block";
+    document.getElementById("AnabolicHormonesSystemic1").style.display = "none";
+    document.getElementById("GrowthHormones1").style.display = "none";
+}
+function function11() {
+    document.getElementById("Acth1").style.display = "none";
+    document.getElementById("AnabolicHormonesSystemic1").style.display = "block";
+    document.getElementById("GrowthHormones1").style.display = "none";
+}
+function function12() {
+    document.getElementById("Acth1").style.display = "none";
+    document.getElementById("AnabolicHormonesSystemic1").style.display = "none";
+    document.getElementById("GrowthHormones1").style.display = "block";
+}
+function function16() {
+    document.getElementById("Vitamins2").style.display = "block";
+    document.getElementById("CalciumProducts1").style.display = "none";
+    document.getElementById("BComplex1").style.display = "block";
+    document.getElementById("OtherNutrients1").style.display = "none";
+    document.getElementById("OtherVitamins1").style.display = "none";
+}
+function function17() {
+    document.getElementById("Vitamins2").style.display = "block";
+    document.getElementById("CalciumProducts1").style.display = "block";
+    document.getElementById("BComplex1").style.display = "none";
+    document.getElementById("OtherNutrients1").style.display = "none";
+    document.getElementById("OtherVitamins1").style.display = "none";
+}
+function function18() {
+    document.getElementById("Vitamins2").style.display = "block";
+    document.getElementById("CalciumProducts1").style.display = "none";
+    document.getElementById("BComplex1").style.display = "none";
+    document.getElementById("OtherNutrients1").style.display = "block";
+    document.getElementById("OtherVitamins1").style.display = "none";
+}
+function function19() {
+    document.getElementById("Vitamins2").style.display = "block";
+    document.getElementById("CalciumProducts1").style.display = "none";
+    document.getElementById("BComplex1").style.display = "none";
+    document.getElementById("OtherNutrients1").style.display = "none";
+    document.getElementById("OtherVitamins1").style.display = "block";
+}
+function function20() {
+    document.getElementById("BloodRelated2").style.display = "block";
+    document.getElementById("Dextran1").style.display = "block";
+    document.getElementById("IronChelatingAgents1").style.display = "none";
+    document.getElementById("ErythropoietinProducts1").style.display = "none";
+}
+function function21() {
+    document.getElementById("BloodRelated2").style.display = "block";
+    document.getElementById("Dextran1").style.display = "none";
+    document.getElementById("IronChelatingAgents1").style.display = "block";
+    document.getElementById("ErythropoietinProducts1").style.display = "none";
+}
+function function22() {
+    document.getElementById("BloodRelated2").style.display = "block";
+    document.getElementById("Dextran1").style.display = "none";
+    document.getElementById("IronChelatingAgents1").style.display = "none";
+    document.getElementById("ErythropoietinProducts1").style.display = "block";
+}
+
